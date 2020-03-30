2b6fd821ec3ffeb3cc5c2764ce0ccd16acf0468a
NicholasAS <anthony.nicholas84@gmail.com>
2020-03-30 19:17:15 +0700

    add Movie Name to cardview, fixed cardview

--- app/src/main/java/com/ppb/kwid/MoviesAdapter.kt
+++ app/src/main/java/com/ppb/kwid/MoviesAdapter.kt
@@ -4,9 +4,11 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.bitmap.CenterCrop
+import org.w3c.dom.Text
 
 class MoviesAdapter(
     private var movies: MutableList<Movie>,
@@ -35,13 +37,14 @@ class MoviesAdapter(
     inner class MovieViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
         private val poster: ImageView = itemView.findViewById(R.id.item_movie_poster)
+        private val tvMovieName: TextView = itemView.findViewById(R.id.item_movie_name)
 
         fun bind(movie: Movie) {
             Glide.with(itemView)
                 .load("https://image.tmdb.org/t/p/w342${movie.posterPath}")
                 .transform(CenterCrop())
                 .into(poster)
-
+            tvMovieName.text = movie.title
             itemView.setOnClickListener { onMovieClick.invoke(movie) }
         }
     }
