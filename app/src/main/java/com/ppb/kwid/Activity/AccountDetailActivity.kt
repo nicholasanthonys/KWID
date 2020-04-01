9ccd9ab63989021e96f63de8ac41416ca6ca2565
NicholasAS <anthony.nicholas84@gmail.com>
2020-04-01 19:05:51 +0700

    Add intent to favorite movie, change button text

--- app/src/main/java/com/ppb/kwid/Activity/AccountDetailActivity.kt
+++ app/src/main/java/com/ppb/kwid/Activity/AccountDetailActivity.kt
@@ -1,13 +1,26 @@
 package com.ppb.kwid.Activity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import com.ppb.kwid.R
 
 class AccountDetailActivity : AppCompatActivity() {
 
+    private  lateinit var btnFavoriteMovies : Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_account_detail)
+        initUI()
+    }
+
+    private fun initUI(){
+        btnFavoriteMovies = findViewById(R.id.btn_favorite_movies)
+        btnFavoriteMovies.setOnClickListener {
+            val intent = Intent(this,FavoriteMoviesActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
