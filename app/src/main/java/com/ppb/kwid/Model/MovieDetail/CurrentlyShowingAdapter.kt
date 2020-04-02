c673c4de053a10561f165dc5e17487a2a71a3030
NicholasAS <anthony.nicholas84@gmail.com>
2020-04-02 17:31:00 +0700

    Add Currently Showing Adapter

---
+++ app/src/main/java/com/ppb/kwid/Model/MovieDetail/CurrentlyShowingAdapter.kt
@@ -0,0 +1,50 @@
+package com.ppb.kwid.Model.MovieDetail
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.ppb.kwid.Model.Movie.Movie
+import com.ppb.kwid.R
+
+class CurrentlyShowingAdapter(
+    private var movies: MutableList<GetMovieDetailsResponse>,
+    private val onMovieClick: (movie: GetMovieDetailsResponse) -> Unit
+) : RecyclerView.Adapter<CurrentlyShowingAdapter.MovieViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MovieViewHolder {
+        val view = LayoutInflater
+            .from(parent.context)
+            .inflate(R.layout.item_movie, parent, false)
+        return MovieViewHolder(view)
+    }
+
+    override fun getItemCount(): Int = movies.size
+
+    override fun onBindViewHolder(holder: MovieViewHolder, position: Int) {
+        holder.bind(movies[position])
+    }
+
+    fun updateMovies(movie: GetMovieDetailsResponse) {
+        this.movies.add(movie)
+        notifyDataSetChanged()
+    }
+
+    inner class MovieViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        private val poster: ImageView = itemView.findViewById(R.id.item_movie_poster)
+        private val tvMovieName: TextView = itemView.findViewById(R.id.item_movie_name)
+
+        fun bind(movie: GetMovieDetailsResponse) {
+            Glide.with(itemView)
+                .load("https://image.tmdb.org/t/p/w342${movie.posterPath}")
+                .centerCrop()
+                .into(poster)
+            tvMovieName.text = movie.title
+            itemView.setOnClickListener { onMovieClick.invoke(movie) }
+
+        }
+    }
+}
