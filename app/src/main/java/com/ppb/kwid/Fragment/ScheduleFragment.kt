e4662733c9612178795efc2728697ceb96414a80
NicholasAS <anthony.nicholas84@gmail.com>
2020-04-08 23:15:11 +0700

    Use String resources for city

--- app/src/main/java/com/ppb/kwid/Fragment/ScheduleFragment.kt
+++ app/src/main/java/com/ppb/kwid/Fragment/ScheduleFragment.kt
@@ -1,11 +1,10 @@
 package com.ppb.kwid.Fragment
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
+import androidx.fragment.app.Fragment
 import com.ppb.kwid.R
 
 /**
