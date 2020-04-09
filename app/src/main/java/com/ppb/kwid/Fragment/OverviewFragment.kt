a708f0c8b86d8ee8588db14c698b478fcdbbd5d0
NicholasAS <anthony.nicholas84@gmail.com>
2020-04-09 20:40:19 +0700

    make textview Scrollable in Overview Fragment

--- app/src/main/java/com/ppb/kwid/Fragment/OverviewFragment.kt
+++ app/src/main/java/com/ppb/kwid/Fragment/OverviewFragment.kt
@@ -1,8 +1,10 @@
 package com.ppb.kwid.Fragment
 
 import android.os.Bundle
+import android.text.method.ScrollingMovementMethod
 import android.view.LayoutInflater
 import android.view.View
+import android.view.View.OnTouchListener
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
@@ -13,13 +15,14 @@ import com.ppb.kwid.R
  * A simple [Fragment] subclass.
  */
 class OverviewFragment : Fragment() {
-    lateinit  var tvOverview : TextView
+    lateinit var tvOverview: TextView
+
     companion object {
-        fun newInstance(overview: String) : OverviewFragment {
+        fun newInstance(overview: String): OverviewFragment {
             val fragment = OverviewFragment()
             val args = Bundle()
             args.putString("overview", overview)
-            fragment.setArguments(args)
+            fragment.arguments = args
             return fragment
         }
     }
@@ -29,16 +32,23 @@ class OverviewFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
+
         return inflater.inflate(R.layout.fragment_overview, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        tvOverview  = view.findViewById(R.id.movie_overview)
+        tvOverview = view.findViewById(R.id.movie_overview)
         tvOverview.text = this.arguments?.getString("overview")
-    }
 
+        tvOverview.setOnTouchListener(OnTouchListener { v, event ->
+            tvOverview.parent.requestDisallowInterceptTouchEvent(true)
+            false
+        })
+
+        tvOverview.movementMethod = ScrollingMovementMethod()
+    }
 
 
 }
