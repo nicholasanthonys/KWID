7d92ed0670ff7c93bc08d8430f82cd6be27f80b9
NicholasAS <anthony.nicholas84@gmail.com>
2020-04-10 15:27:38 +0700

    Add Result class

---
+++ app/src/main/java/com/ppb/kwid/Model/Video/Result.kt
@@ -0,0 +1,14 @@
+package com.ppb.kwid.Model.Video
+
+import com.google.gson.annotations.SerializedName
+
+data class Result(
+    @SerializedName("id") val id: String,
+    @SerializedName("iso_639_1") val iso_639_1: String,
+    @SerializedName("iso_3166_1") val iso_3166_1: String,
+    @SerializedName("key") val key: String,
+    @SerializedName("name") val name: String,
+    @SerializedName("site") val site: String,
+    @SerializedName("size") val size: Int,
+    @SerializedName("type") val type: String
+)
