745c3ba4b3ba88b35c8c26af0013107de2202b5b
NicholasAS <anthony.nicholas84@gmail.com>
2020-04-11 15:00:31 +0700

    Allow key variable to be null -> String?

--- app/src/main/java/com/ppb/kwid/Model/Video/Result.kt
+++ app/src/main/java/com/ppb/kwid/Model/Video/Result.kt
@@ -6,7 +6,7 @@ data class Result(
     @SerializedName("id") val id: String,
     @SerializedName("iso_639_1") val iso_639_1: String,
     @SerializedName("iso_3166_1") val iso_3166_1: String,
-    @SerializedName("key") val key: String,
+    @SerializedName("key") val key: String? = "",
     @SerializedName("name") val name: String,
     @SerializedName("site") val site: String,
     @SerializedName("size") val size: Int,
